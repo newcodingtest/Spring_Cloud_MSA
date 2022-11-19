4ce27b72d7376caf0a4924a76d4aa3b65d46d480
newcodingtest <pulpul8282@naver.com>
2022-11-20 01:37:50 +0900

    주문 설정 추가

---
+++ Spring_Cloud_MSA/order-service/src/test/java/com/yoon/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
